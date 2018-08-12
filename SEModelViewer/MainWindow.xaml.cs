f073562126242cdadf7c07ebdfa1f3b1d7aac836
Scobalula <Philip@DESKTOP-PL8DU6U>
2018-08-12 14:37:09 +0100

    Add project files.

---
+++ SEModelViewer/MainWindow.xaml.cs
@@ -0,0 +1,277 @@
+﻿/*
+    Copyright (c) 2018 Philip/Scobalula - SEModelViewer
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace SEModelViewer
+{
+    /// <summary>
+    /// Model Class
+    /// </summary>
+    public class ModelFile : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Model Bone Data
+        /// </summary>
+        public class ModelBone
+        {
+            public string Name { get; set; }
+
+            public string Position { get; set; }
+        }
+
+        /// <summary>
+        /// File Path
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// File Name without extension
+        /// </summary>
+        public string Name { get { return System.IO.Path.GetFileNameWithoutExtension(Path); } }
+
+        /// <summary>
+        /// Models Bones
+        /// </summary>
+        public List<ModelBone> ModeBones { get; set; }
+
+        /// <summary>
+        /// Bone Count Value
+        /// </summary>
+        private int BoneCountValue { get; set; }
+
+        /// <summary>
+        /// Number of Bones
+        /// </summary>
+        public int BoneCount
+        {
+            get
+            {
+                return BoneCountValue;
+            }
+            set
+            {
+                if (BoneCountValue != value)
+                {
+                    BoneCountValue = value;
+                    NotifyPropertyChanged("BoneCount");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Loads a single model
+        /// </summary>
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog()
+            {
+                Filter = "SEModel File (*.semodel)|*.semodel"
+            };
+
+            if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(openFileDialog.FileName))
+            {
+                ModelFile modelFile = new ModelFile() { Path = openFileDialog.FileName };
+                LoadModel(modelFile);
+            }
+        }
+
+        /// <summary>
+        /// Loads folder of OBJ Files
+        /// </summary>
+        private void LoadFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new FolderBrowserDialog();
+
+            if (folderDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK && !string.IsNullOrWhiteSpace(folderDialog.SelectedPath))
+            {
+                ModelList.Items.Clear();
+
+                string[] files = Directory.GetFiles(folderDialog.SelectedPath, "*.semodel*", SearchOption.AllDirectories);
+
+                foreach(string file in files)
+                {
+                    ModelFile model = new ModelFile()
+                    {
+                        Path = file,
+                        BoneCount = 0,
+                    };
+
+                    ModelList.Items.Add(model);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Switches model on index change/click.
+        /// </summary>
+        private void ModelList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var model = (ModelFile)ModelList.SelectedItem;
+
+            LoadModel(model);
+        }
+
+        /// <summary>
+        /// Loads an OBJ File, if it fails the model is not displayed.
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void LoadModel(ModelFile modelFile)
+        {
+            try
+            {
+                var watch            = Stopwatch.StartNew();
+                SEModelImporter reader  = new SEModelImporter();
+                reader.LoadTextures = LoadTextures.IsChecked == true;
+                reader.Folder = Path.GetDirectoryName(modelFile.Path);
+
+                using (FileStream stream = new FileStream(modelFile.Path, FileMode.Open))
+                {
+                    Model3DGroup loadedModel = reader.Read(stream);
+                    modelFile.ModeBones = reader.ModelBones;
+                    model.Content = loadedModel;
+                    MainViewport.ZoomExtents(0);
+                }
+                watch.Stop();
+                Status.Content        = string.Format("Status     : Loaded {0} in {1} Seconds", modelFile.Name, watch.ElapsedMilliseconds / 1000.0);
+                VertexCount.Content   = string.Format("Vertices   : {0}", reader.VertexCount);
+                FaceCount.Content     = string.Format("Faces      : {0}", reader.FaceCount);
+                MaterialCount.Content = string.Format("Materials  : {0}", reader.MaterialCount);
+                BoneCount.Content     = string.Format("Bones      : {0}", reader.BoneCount);
+                Status.Foreground     = new SolidColorBrush(Colors.Lime);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Status.Content        = string.Format("Status     : Error - {0}", e.Message);
+                VertexCount.Content   = string.Format("Vertices   : None");
+                FaceCount.Content     = string.Format("Faces      : None");
+                MaterialCount.Content = string.Format("Materials  : None");
+                BoneCount.Content     = string.Format("Bones      : None");
+                Status.Foreground     = new SolidColorBrush(Colors.Red);
+                model.Content         = null;
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Copies model name to clipboard
+        /// </summary>
+        private void CopyAssetName_Click(object sender, RoutedEventArgs e)
+        {
+            ModelFile model = (ModelFile)ModelList.SelectedItem;
+
+            System.Windows.Clipboard.SetText(model.Name);
+        }
+        
+        /// <summary>
+        /// Opens About Window
+        /// </summary>
+        private void AboutButton_Click(object sender, RoutedEventArgs e)
+        {
+            new AboutWindow()
+            {
+                Owner = this
+            }.ShowDialog();
+        }
+        
+        /// <summary>
+        /// Loads Model Bone Counts
+        /// </summary>
+        private void LoadModelInfo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                foreach (var item in ModelList.Items)
+                {
+                    ModelFile modelFile = item as ModelFile;
+
+                    using (BinaryReader reader = new BinaryReader(new FileStream(modelFile.Path, FileMode.Open)))
+                    {
+                        reader.BaseStream.Seek(14, SeekOrigin.Begin);
+                        modelFile.BoneCount = reader.ReadInt32();
+                    }
+                }
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Shows Bone Info Window with Bone Names and Positions
+        /// </summary>
+        private void BonesButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ModelList.SelectedItems.Count == 0)
+            {
+                System.Windows.MessageBox.Show("No model selected", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            BonesWindow bonesWindow = new BonesWindow();
+
+            ModelFile modelFile = ModelList.SelectedItem as ModelFile;
+
+            foreach(var bone in modelFile.ModeBones)
+            {
+                bonesWindow.Bones.Items.Add(bone);
+            }
+
+            bonesWindow.Owner = this;
+            bonesWindow.ShowDialog();
+        }
+    }
+}
---
+++ SEModelViewer/SEModelImporter.cs
@@ -0,0 +1,250 @@
+﻿/*
+    Copyright (c) 2018 Philip/Scobalula - SEModelViewer
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using SELib;
+using HelixToolkit.Wpf;
+
+namespace SEModelViewer
+{
+    /// <summary>
+    /// SEModel Helix Importer
+    /// </summary>
+    class SEModelImporter : ModelReader
+    {
+        /// <summary>
+        /// Number of Materials
+        /// </summary>
+        public int MaterialCount { get; set; }
+
+        /// <summary>
+        /// Number of Vertices
+        /// </summary>
+        public uint VertexCount { get; set; }
+
+        /// <summary>
+        /// Number of Faces
+        /// </summary>
+        public uint FaceCount { get; set; }
+
+        /// <summary>
+        /// Number of Bones
+        /// </summary>
+        public uint BoneCount { get; set; }
+
+        /// <summary>
+        /// Load Textures from Material Data
+        /// </summary>
+        public bool LoadTextures { get; set; }
+
+        /// <summary>
+        /// Folder Path (For material loader)
+        /// </summary>
+        public string Folder { get; set; }
+
+        /// <summary>
+        /// Random Int (For material loader)
+        /// </summary>
+        private readonly Random RandomInt = new Random();
+
+        /// <summary>
+        /// Bones in this Model
+        /// </summary>
+        public List<ModelFile.ModelBone> ModelBones { get; set; }
+
+        /// <summary>
+        /// SEModel Materials
+        /// </summary>
+        private readonly List<Material> Materials = new List<Material>();
+
+        /// <summary>
+        /// SEModel Meshes
+        /// </summary>
+        private readonly List<Mesh> Meshes = new List<Mesh>();
+
+        /// <summary>
+        /// Loads SEModel
+        /// </summary>
+        public override Model3DGroup Read(Stream s)
+        {
+            SEModel semodel = SEModel.Read(s);
+
+            Model3DGroup modelGroup = new Model3DGroup();
+
+            MaterialCount = semodel.Materials.Count;
+            BoneCount = semodel.BoneCount;
+
+
+            ModelBones = new List<ModelFile.ModelBone>();
+
+            LoadBones(semodel);
+
+            LoadMaterials(semodel);
+
+            foreach(var semesh in semodel.Meshes)
+            {
+                Mesh mesh = new Mesh
+                {
+                    Positions = new List<Point3D>(),
+                    TriangleIndices = new List<int>(),
+                    TextureCoordinates = new List<Point>(),
+                    Normals = new List<Vector3D>(),
+                    Material = Materials[semesh.MaterialReferenceIndicies[0]]
+                };
+
+                VertexCount += semesh.VertexCount;
+                FaceCount += semesh.FaceCount;
+
+                foreach(var vertex in semesh.Verticies)
+                {
+                    mesh.Positions.Add(new Point3D(vertex.Position.X, vertex.Position.Y, vertex.Position.Z));
+                    mesh.TextureCoordinates.Add(new Point(vertex.UVSets[0].X, vertex.UVSets[0].Y));
+                    mesh.Normals.Add(new Vector3D(vertex.VertexNormal.X, vertex.VertexNormal.Y, vertex.VertexNormal.Z));
+                }
+
+                foreach(var face in semesh.Faces )
+                {
+                    mesh.TriangleIndices.Add((int)face.FaceIndex1);
+                    mesh.TriangleIndices.Add((int)face.FaceIndex2);
+                    mesh.TriangleIndices.Add((int)face.FaceIndex3);
+                }
+
+                modelGroup.Children.Add(mesh.CreateModel());
+            }
+
+            return modelGroup;
+        }
+
+        /// <summary>
+        /// Loads Bone Names and Offsets (As a string formatted)
+        /// </summary>
+        private void LoadBones(SEModel semodel)
+        {
+            foreach(var bone in semodel.Bones)
+            {
+                ModelBones.Add(new ModelFile.ModelBone()
+                {
+                    Name = bone.BoneName,
+                    Position = String.Format("({0}, {1}, {2})", bone.GlobalPosition.X, bone.GlobalPosition.Y, bone.GlobalPosition.Z)
+                });
+            }
+        }
+
+        /// <summary>
+        /// Loads materials and textures (if they exist)
+        /// </summary>
+        private void LoadMaterials(SEModel semodel)
+        {
+            foreach(var material in semodel.Materials)
+            {
+                var materialGroup = new MaterialGroup();
+
+                var data = material.MaterialData as SEModelSimpleMaterial;
+
+                string image = Path.Combine(Folder, data.DiffuseMap);
+
+                if (File.Exists(image) && LoadTextures == true)
+                {
+                    materialGroup.Children.Add(new DiffuseMaterial(CreateTextureBrush(image)));
+                }
+                else
+                {
+
+                    materialGroup.Children.Add(new DiffuseMaterial(new SolidColorBrush(Color.FromRgb
+                        (
+                            (byte)RandomInt.Next(128, 255),
+                            (byte)RandomInt.Next(128, 255),
+                            (byte)RandomInt.Next(128, 255)
+                        ))));
+                }
+
+                Materials.Add(materialGroup);
+            }
+        }
+
+        /// <summary>
+        /// Loads texture
+        /// </summary>
+        private ImageBrush CreateTextureBrush(string path)
+        {
+            var img = new BitmapImage(new Uri(path, UriKind.Relative));
+            var textureBrush = new ImageBrush(img) { Opacity = 1.0, ViewportUnits = BrushMappingMode.Absolute, TileMode = TileMode.Tile };
+            return textureBrush;
+        }
+
+        /// <summary>
+        /// Mesh Data
+        /// </summary>
+        private class Mesh
+        {
+            /// <summary>
+            /// Vertex Positions
+            /// </summary>
+            public List<Point3D> Positions { get; set; }
+
+            /// <summary>
+            /// Face Indices
+            /// </summary>
+            public List<int> TriangleIndices { get; set; }
+
+            /// <summary>
+            /// UV Positions
+            /// </summary>
+            public List<Point> TextureCoordinates { get; set; }
+
+            /// <summary>
+            /// Vertex Normals
+            /// </summary>
+            public List<Vector3D> Normals { get; set; }
+
+            /// <summary>
+            /// Mesh Material
+            /// </summary>
+            public Material Material { get; set; }
+
+            /// <summary>
+            /// Creates a Model from Mesh Data
+            /// </summary>
+            /// <returns></returns>
+            public Model3D CreateModel()
+            {
+                var geometry = new MeshGeometry3D
+                {
+                    Positions = new Point3DCollection(Positions),
+                    TriangleIndices = new Int32Collection(TriangleIndices),
+                    Normals = new Vector3DCollection(Normals)
+                };
+                if (TextureCoordinates != null)
+                {
+                    geometry.TextureCoordinates = new PointCollection(TextureCoordinates);
+                }
+
+                return new GeometryModel3D(geometry, Material) { BackMaterial = Material };
+            }
+        }
+    }
+}
